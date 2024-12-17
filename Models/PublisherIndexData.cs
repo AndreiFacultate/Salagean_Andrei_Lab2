400458b22f96945964cceaef87ceb9ffeaa0a785
ANDREI-MARIUS SĂLĂGEAN <andrei.marius.salagean@stud.ubbcluj.ro>
2024-12-17 17:20:26 +0200

    Laborator_4

---
+++ Models/PublisherIndexData.cs
@@ -0,0 +1,9 @@
+﻿using Salagean_Andrei_Lab2.Models;
+namespace Salagean_Andrei_Lab2.Models
+{
+    public class PublisherIndexData
+    {
+        public IEnumerable<Publisher> Publishers { get; set; }
+        public IEnumerable<Book> Books { get; set; }
+    }
+}
